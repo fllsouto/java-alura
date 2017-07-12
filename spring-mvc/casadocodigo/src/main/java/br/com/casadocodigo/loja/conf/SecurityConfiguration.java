66c37a481f5daff904e9d9b312f3e7fb9e9cee56
Fellipe Souto <fllsouto@gmail.com>
2017-07-12 11:16:32 -0300

    Added login page with spring security

--- spring-mvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ spring-mvc/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,18 +1,13 @@
-//package br.com.casadocodigo.loja.conf;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 //import br.com.casadocodigo.loja.daos.UsuarioDAO;
-//
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//    
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
 //    @Autowired
 //    private UsuarioDAO usuarioDao;
 //
@@ -34,5 +29,5 @@
 //        auth.userDetailsService(usuarioDao)
 //        .passwordEncoder(new BCryptPasswordEncoder());
 //    }
-//
-//}
+
+}
